02840a239e509a205454233d5c49fb4972f91f86
Lucas Vanni <lucas.vanni@hotmail.com>
2020-08-28 12:11:24 -0300

    Migrations and Creation Model to send a Database

---
+++ Bootcamp/goBarber/backend/src/database/migrations/1598559010639-CreateAppointments.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1598559010639
+    implements MigrationInterface {
+    // O que queremos que seja feito no banco de dados, quando a migration for executada
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'appointments',
+                columns: [
+                    {
+                        // Nome da coluna
+                        name: 'id',
+                        // Tipo da coluna
+                        type: 'varchar',
+                        // Chave primária
+                        isPrimary: true,
+                        /* Estratégia de geração, para gerar o campo
+                            automáticamente com o objetivo de ser um
+                            uuid
+                        */
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'provider',
+                        type: 'varchar',
+                        // Não é possível ter valores nulos
+                        isNullable: false,
+                    },
+                    {
+                        name: 'Date',
+                        /*
+                            Se não estiver utilizando postgres
+                            podemos utilizar somente timestamp,
+                            porém no timestamp with time zone,
+                            ele salva a data com o fuzo horário
+                            junto.
+                        */
+                        type: 'timestamp with time zone',
+                        isNullable: false,
+                    },
+                ],
+            }),
+        );
+    }
+
+    // Utilizada como um fallback (desfazer o que fiz no método up)
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments');
+    }
+}
--- Bootcamp/goBarber/backend/src/models/Appointment.ts
+++ Bootcamp/goBarber/backend/src/models/Appointment.ts
@@ -1,11 +1,31 @@
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
+// Comunica para o banco que esse model está relacionado a uma tabela
+// Entity => Algo que irá ser salvo no banco de dados, uma entidade
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 // Responsável pelo formato dos dados que serão salvo.
+/*
+    Decorator Entity funciona como uma função,
+    ele irá pegar a função entity e como parâmetro
+    vai enviar a classe que está em baixo
+*/
+// Toda vez que faz as operações será salvo na tabela appointments
+@Entity('appointments')
 class Appointment {
+    // Decorator para chave primária gerada automáticamente.
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    /*
+        Decorator para coluna comum,
+        ele quando utilizado
+        sem passar parâmetros
+        assume o valor varchar
+    */
+    @Column()
     provider: string;
 
+    @Column('time with time zone')
     date: Date;
 
     /*
@@ -13,14 +33,15 @@ class Appointment {
         (new Appointment()) consiga passar parâmetros para
         ela e utilizá-los dentro da classe.
     */
-    constructor({ provider, date }: Omit<Appointment, 'id'>) {
-        // Sempre será um id gerado automáticamente e único
-        this.id = uuid();
+    // // Não precisamos do constructor no model com o typeORM
+    // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+    //     // Sempre será um id gerado automáticamente e único
+    //     this.id = uuid();
 
-        // Setando as variáveis com esse nome
-        this.provider = provider;
-        this.date = date;
-    }
+    //     // Setando as variáveis com esse nome
+    //     this.provider = provider;
+    //     this.date = date;
+    // }
 }
 
 // Faz com que o appointment possa ser acessado por outros arquivos
