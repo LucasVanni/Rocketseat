c7eeceba1faab5aa5a4ef8933842eae3f12c3b9f
LucasVanni <lucas.vanni@hotmail.com>
2020-10-04 23:07:30 -0300

    Chore: Correções

--- Bootcamp/goBarber/backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ Bootcamp/goBarber/backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+import { classToClass } from 'class-transformer';
 
 export default class ProvidersController {
     public async index(req: Request, res: Response): Promise<Response> {
@@ -13,6 +14,6 @@ export default class ProvidersController {
             user_id,
         });
 
-        return res.json(providers);
+        return res.json(classToClass(providers));
     }
 }
