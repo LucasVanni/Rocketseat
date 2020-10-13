ddd480d1cd4f6db35fc14f0a2d37aff967174053
LucasVanni <lucas.vanni@hotmail.com>
2020-10-13 06:09:59 -0300

    Fix: APP

--- Bootcamp/goBarber/mobile/src/routes/app.routes.tsx
+++ Bootcamp/goBarber/mobile/src/routes/app.routes.tsx
@@ -10,20 +10,20 @@ import AppointmentCreated from '../pages/AppointmentCreated';
 const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
-  <App.Navigator
-    screenOptions={{
-      headerShown: false,
-      cardStyle: { backgroundColor: '#312e38' },
-    }}
-  >
-    <App.Screen name="Dashboard" component={Dashboard} />
-    <App.Screen name="Profile" component={Profile} />
-    <App.Screen
-      name="AppointmentDatePicker"
-      component={AppointmentDatePicker}
-    />
-    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
-  </App.Navigator>
+    <App.Navigator
+        screenOptions={{
+            headerShown: false,
+            cardStyle: { backgroundColor: '#312e38' },
+        }}
+    >
+        <App.Screen name="Dashboard" component={Dashboard} />
+        <App.Screen name="Profile" component={Profile} />
+        <App.Screen
+            name="AppointmentDatePicker"
+            component={AppointmentDatePicker}
+        />
+        <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+    </App.Navigator>
 );
 
 export default AppRoutes;
