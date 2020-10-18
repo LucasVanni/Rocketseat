af8d57a6765232f91588119efdbe2c0b48d3dead
LucasVanni <lucas.vanni@hotmail.com>
2020-10-18 18:13:04 -0300

    Criando servidor JSON e recuperando dados dele

--- redux-class/src/components/Catalog.tsx
+++ redux-class/src/components/Catalog.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
+import { IProduct } from '../store/modules/cart/types';
+// import { useSelector } from 'react-redux';
 
 const Catalog: React.FC = () => {
-  const catalog = useSelector(state => state);
-
-  console.log(catalog);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
   
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data);
+    });
+  }, []);
+
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+    
+      {catalog.map(product => (
+        <article key={product.id} >
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {" "}
+
+          <button type="button" >Comprar</button>
+        </article>
+      ))}
+    </main>
   );
 };
 
