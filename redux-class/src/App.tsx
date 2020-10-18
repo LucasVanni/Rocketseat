c4d6d687c1fb7471de060e2f4b5ee9e857cbce8f
LucasVanni <lucas.vanni@hotmail.com>
2020-10-18 17:30:39 -0300

    Feature: Configurando o store do redux

--- redux-class/src/App.tsx
+++ redux-class/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import Catalog from './components/Catalog';
+import store from './store';
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+  )
 }
 
 export default App;
