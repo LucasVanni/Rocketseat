63fde147571eaa44aab1d8968f82b358650fc774
LucasVanni <lucas.vanni@hotmail.com>
2020-10-19 08:29:00 -0300

    Tipagens Redux

--- redux-class/src/components/Cart.tsx
+++ redux-class/src/components/Cart.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { IState } from '../store';
+import { ICartItem } from '../store/modules/cart/types';
 
 const Cart: React.FC = () => {
-  const state = useSelector(state => state);
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items);
 
-  console.log(state);
+  console.log(cart);
 
   return (
     <table>
@@ -17,7 +19,14 @@ const Cart: React.FC = () => {
         </tr>
       </thead>
       <tbody>
-
+        {cart.map(item => (
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+          </tr>
+        ))}
       </tbody>  
     </table>
   )
