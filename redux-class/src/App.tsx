11a727c70ecfeb01afe08e794529df0cae36089f
LucasVanni <lucas.vanni@hotmail.com>
2020-10-19 08:17:55 -0300

    Alterando estados

--- redux-class/src/App.tsx
+++ redux-class/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Catalog from './components/Catalog';
+import Cart from './components/Cart';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   )
 }
---
+++ redux-class/src/components/Cart.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Cart: React.FC = () => {
+  const state = useSelector(state => state);
+
+  console.log(state);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>  
+    </table>
+  )
+}
+
+export default Cart;
