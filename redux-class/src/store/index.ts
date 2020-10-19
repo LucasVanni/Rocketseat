3c6c7d2a6fabdef13b2e5bbdb40fb2cd3a1e7b08
LucasVanni <lucas.vanni@hotmail.com>
2020-10-19 08:45:27 -0300

    Dev tools debug

--- redux-class/src/store/index.ts
+++ redux-class/src/store/index.ts
@@ -1,12 +1,17 @@
 import { createStore } from 'redux';
-import { ICartState } from './modules/cart/types';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './modules/rootReducer';
+
+import { ICartState } from './modules/cart/types';
 
 export interface IState {
   cart: ICartState;
 }
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()  
+);
 
 export default store;
